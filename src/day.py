b1b19903109295f199ebaffd2953c5f05033f9d8
J3FALL <crisp.coad@gmail.com>
2018-08-27 20:00:33 +0300

    Implement check_for_absence case

--- src/day.py
+++ src/day.py
@@ -1,4 +1,3 @@
-
 class ExperimentDay:
     def __init__(self, date, ice_file=None, tracers_file=None, currents_file=None):
         self.date = date
@@ -17,3 +16,7 @@ class ExperimentDay:
         return self.date != other.date or self.ice_file != other.ice_file or \
                self.tracers_file != other.tracers_file or \
                self.currents_file != other.currents_file
+
+    def __str__(self):
+        return "date: %s, ice_file: %s, tracers_file: %s, currents_file: %s" % \
+               (self.date.strftime("%Y%m%d"), self.ice_file, self.tracers_file, self.currents_file)
--- src/experiment.py
+++ src/experiment.py
@@ -2,22 +2,27 @@ from src.valid import ValidResults
 
 
 class Experiment:
-    def __init__(self, date_from, date_to):
+    def __init__(self, date_from, date_to, results_by_days):
         self._date_from = date_from
         self._date_to = date_to
-        self._results_by_day = []
+        self._results_by_days = results_by_days
+        self._results_by_days.sort(key=lambda day: day.date)
 
     def check_for_absence(self):
         '''
         Check whether results contain all days and each day contains
         three corresponding resulted files: [ice, tracers, currents]
-        :return:
+        :return: False if all resulted files are presented, otherwise exceptions were raised
         '''
 
-        raise NotImplementedError
         valid_results = ValidResults().generate(self._date_from, self._date_to)
 
-        assert len(self._results_by_day) == len(valid_results)
+        for valid, given in zip(valid_results, self._results_by_days):
+            if valid.date != given.date:
+                error = "Simulation results were not found for day: %s" % valid.date.strftime("%Y%m%d")
+                raise Exception(error)
+            elif valid != given:
+                error = "Simulation results for day: %s have some missing files or its names are incorrect: %s" % given
+                raise Exception(error)
 
-        for valid, given in zip(valid_results, self._results_by_day):
-            print(valid, given)
+        return False
