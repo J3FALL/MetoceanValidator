71dc70beb0f0cc3fdc4be0028a23f5fa4c50da21
J3FALL <crisp.coad@gmail.com>
2018-08-29 12:17:13 +0300

    Change currents file format

--- test/experiment_test.py
+++ test/experiment_test.py
@@ -9,13 +9,13 @@ class ExperimentTest(unittest.TestCase):
     def test_check_for_absence_correct(self):
         results_by_days = [ExperimentDay(date=date(2013, 1, 2), ice_file='ARCTIC_1h_ice_grid_TUV_20130102-20130102.nc',
                                          tracers_file='ARCTIC_1h_T_grid_T_20130102-20130102.nc',
-                                         currents_file='ARCTIC_1h_T_UV_20130102-20130102.nc'),
+                                         currents_file='ARCTIC_1h_UV_grid_UV_20130102-20130102.nc'),
                            ExperimentDay(date=date(2013, 1, 3), ice_file='ARCTIC_1h_ice_grid_TUV_20130103-20130103.nc',
                                          tracers_file='ARCTIC_1h_T_grid_T_20130103-20130103.nc',
-                                         currents_file='ARCTIC_1h_T_UV_20130103-20130103.nc'),
+                                         currents_file='ARCTIC_1h_UV_grid_UV_20130103-20130103.nc'),
                            ExperimentDay(date=date(2013, 1, 1), ice_file='ARCTIC_1h_ice_grid_TUV_20130101-20130101.nc',
                                          tracers_file='ARCTIC_1h_T_grid_T_20130101-20130101.nc',
-                                         currents_file='ARCTIC_1h_T_UV_20130101-20130101.nc')
+                                         currents_file='ARCTIC_1h_UV_grid_UV_20130101-20130101.nc')
                            ]
         experiment = Experiment(date_from=date(2013, 1, 1), date_to=date(2013, 1, 3), results_by_days=results_by_days)
 
@@ -24,10 +24,10 @@ class ExperimentTest(unittest.TestCase):
     def test_check_for_absence_missing_day(self):
         results_by_days = [ExperimentDay(date=date(2013, 1, 2), ice_file='ARCTIC_1h_ice_grid_TUV_20130102-20130102.nc',
                                          tracers_file='ARCTIC_1h_T_grid_T_20130102-20130102.nc',
-                                         currents_file='ARCTIC_1h_T_UV_20130102-20130102.nc'),
+                                         currents_file='ARCTIC_1h_UV_grid_UV_20130102-20130102.nc'),
                            ExperimentDay(date=date(2013, 1, 3), ice_file='ARCTIC_1h_ice_grid_TUV_20130103-20130103.nc',
                                          tracers_file='ARCTIC_1h_T_grid_T_20130103-20130103.nc',
-                                         currents_file='ARCTIC_1h_T_UV_20130103-20130103.nc')
+                                         currents_file='ARCTIC_1h_UV_grid_UV_20130103-20130103.nc')
                            ]
         experiment = Experiment(date_from=date(2013, 1, 1), date_to=date(2013, 1, 3), results_by_days=results_by_days)
         with self.assertRaises(Exception) as cm:
@@ -39,7 +39,7 @@ class ExperimentTest(unittest.TestCase):
     def test_check_for_absence_missing_file(self):
         results_by_days = [ExperimentDay(date=date(2013, 1, 1), ice_file=None,
                                          tracers_file='ARCTIC_1h_T_grid_T_20130101-20130101.nc',
-                                         currents_file='ARCTIC_1h_T_UV_20130101-20130101.nc')
+                                         currents_file='ARCTIC_1h_UV_grid_UV_20130101-20130101.nc')
                            ]
         experiment = Experiment(date_from=date(2013, 1, 1), date_to=date(2013, 1, 1), results_by_days=results_by_days)
         with self.assertRaises(Exception) as cm:
--- test/valid_test.py
+++ test/valid_test.py
@@ -14,13 +14,13 @@ class ValidResultsTest(unittest.TestCase):
         date_to = date(2013, 1, 3)
         expected_results = [ExperimentDay(date=date(2013, 1, 1), ice_file='ARCTIC_1h_ice_grid_TUV_20130101-20130101.nc',
                                           tracers_file='ARCTIC_1h_T_grid_T_20130101-20130101.nc',
-                                          currents_file='ARCTIC_1h_T_UV_20130101-20130101.nc'),
+                                          currents_file='ARCTIC_1h_UV_grid_UV_20130101-20130101.nc'),
                             ExperimentDay(date=date(2013, 1, 2), ice_file='ARCTIC_1h_ice_grid_TUV_20130102-20130102.nc',
                                           tracers_file='ARCTIC_1h_T_grid_T_20130102-20130102.nc',
-                                          currents_file='ARCTIC_1h_T_UV_20130102-20130102.nc'),
+                                          currents_file='ARCTIC_1h_UV_grid_UV_20130102-20130102.nc'),
                             ExperimentDay(date=date(2013, 1, 3), ice_file='ARCTIC_1h_ice_grid_TUV_20130103-20130103.nc',
                                           tracers_file='ARCTIC_1h_T_grid_T_20130103-20130103.nc',
-                                          currents_file='ARCTIC_1h_T_UV_20130103-20130103.nc')
+                                          currents_file='ARCTIC_1h_UV_grid_UV_20130103-20130103.nc')
                             ]
 
         all_results = results.generate(date_from, date_to)
