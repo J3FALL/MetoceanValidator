ec1bc645997d2ec99589b1616cd6bd4e6785ae46
J3FALL <crisp.coad@gmail.com>
2018-09-03 13:50:06 +0300

    Add check for presence of all variables of NetCDF

--- src/netcdf.py
+++ src/netcdf.py
@@ -2,6 +2,8 @@ import logging
 
 from netCDF4 import Dataset as NetCDF
 
+from src.name_format import NameFormat
+
 
 class NCFile:
     def __init__(self, name, path, type):
@@ -20,3 +22,19 @@ class NCFile:
             logging.error(error)
 
         return error
+
+    def check_variables(self):
+        nc_file = NetCDF(self.path)
+        nf = NameFormat()
+
+        errors = []
+        for correct_var in nf.variables(self.type):
+            try:
+                var = nc_file.variables[correct_var]
+            except KeyError:
+                error = "%s variable is not presented in %s" % (correct_var, self.name)
+                logging.error(error)
+                errors.append(error)
+
+        print("%s check_variables: done" % self.name)
+        return errors
