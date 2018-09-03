389b6d8a62a655ade9f59a17f053001b9d8fc46c
J3FALL <crisp.coad@gmail.com>
2018-09-03 18:59:59 +0300

    Cleanup refined script

--- src/refined.py
+++ src/refined.py
@@ -1,9 +1,6 @@
-import sys
 from datetime import date
 
 from src.blade import BladeChecker
-
-sys.path.append('/home/hpc-rosneft/nemo-error-checker/')
 
 checker = BladeChecker(date_from=date(1964, 1, 1), date_to=date(2015, 12, 31))
 checker.check_local_storage()
