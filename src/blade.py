a937c37f513131a7923784247d010d5ca383b13d
J3FALL <crisp.coad@gmail.com>
2018-11-01 17:14:24 +0300

    Add summary log dumping

--- src/blade.py
+++ src/blade.py
@@ -48,21 +48,29 @@ class BladeChecker:
     # TODO: extract class or/and Error class
     def summary(self, absence_errors, vars_errors):
         self.dump_matching_errors()
-        print("absence errors total: %d, including:" % len(absence_errors))
-        no_matching = list(filter(lambda error: error if "no matching type" in error else None, absence_errors))
-        not_found_days = list(filter(lambda error: error if "were not found" in error else None, absence_errors))
-        not_found_files = list(filter(lambda error: error if "some missing files" in error else None, absence_errors))
-        print("     no matching type: %d" % len(no_matching))
-        print("      missing days: %d" % len(not_found_days))
-        print("      missing files/incorrect file names: %d" % len(not_found_files))
 
-        print("variable errors total: %d, including:" % len(vars_errors))
-        with_constants = list(filter(lambda error: error if "constant value" in error else None, vars_errors))
-        wrong_shape = list(filter(lambda error: error if "doesn't correspond to" in error else None, vars_errors))
-        missing_vars = list(filter(lambda error: error if "variable is not presented" in error else None, vars_errors))
-        print("     with constant values: %d" % len(with_constants))
-        print("     with wrong shape: %d" % len(wrong_shape))
-        print("     with missing variable: %d" % len(missing_vars))
+        with open('../logs/errors_summary.log', 'w') as file:
+            file.write("absence errors total: %d, including:\n" % len(absence_errors))
+
+            no_matching = list(filter(lambda error: error if "no matching type" in error else None, absence_errors))
+            not_found_days = list(filter(lambda error: error if "were not found" in error else None, absence_errors))
+            not_found_files = list(
+                filter(lambda error: error if "some missing files" in error else None, absence_errors))
+
+            file.write('\tno matching type: %d\n' % len(no_matching))
+            file.write('\tmissing days: %d\n' % len(not_found_days))
+            file.write('\tmissing files/incorrect file names: %d\n' % len(not_found_files))
+
+            file.write('variable errors total: %d, including:\n' % len(vars_errors))
+
+            with_constants = list(filter(lambda error: error if "constant value" in error else None, vars_errors))
+            wrong_shape = list(filter(lambda error: error if "doesn't correspond to" in error else None, vars_errors))
+            missing_vars = list(
+                filter(lambda error: error if "variable is not presented" in error else None, vars_errors))
+
+            file.write('\twith constant values: %d\n' % len(with_constants))
+            file.write('\twith wrong shape: %d\n' % len(wrong_shape))
+            file.write('\twith missing variable: %d\n' % len(missing_vars))
 
     def dump_matching_errors(self):
         with open('../logs/matching_errors.log', 'w') as file:
