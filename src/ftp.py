7ce4ca93186c584304c8833ca9f0601596701a52
J3FALL <crisp.coad@gmail.com>
2018-11-12 14:56:58 +0300

    Add missed year files download from FtpStorage

--- src/ftp.py
+++ src/ftp.py
@@ -1,11 +1,13 @@
 import ftplib
+import os
 
 import yaml
+from tqdm import tqdm
 
 
 class FtpStorage:
     def __init__(self):
-        self.storages = self.init_storages()
+        self.storages, self.storage_to_dirs = self.init_storages()
         self.dirs = self.init_directories()
         self.credentials = self.init_credentials()
 
@@ -23,7 +25,14 @@ class FtpStorage:
 
     def init_storages(self):
         config = self.load_config()
-        return [storage for storage in config['storages']]
+        storages = []
+        storage_to_dirs = {}
+
+        for storage in config['storages']:
+            storages.append(storage['ip'])
+            storage_to_dirs[storage['ip']] = storage['mount_dir']
+
+        return storages, storage_to_dirs
 
     def init_credentials(self):
         config = self.load_config()
@@ -61,9 +70,47 @@ class FtpStorage:
 
         return files
 
+    def download_year(self, year, path_to_download=''):
+
+        dir = next(d for d in self.dirs if d.year == year)
+        connect = ftplib.FTP(host=dir.ip, user=self.credentials['user'], passwd=self.credentials['pass'])
+
+        files = []
+        try:
+            connect.cwd(dir.path)
+            files = connect.nlst()
+        except Exception:
+            print("error has occurred")
+
+        for file in tqdm(files):
+            host_file = os.path.join(path_to_download, file)
+            with open(host_file, 'wb') as local_file:
+                connect.retrbinary('RETR ' + file, local_file.write)
+
 
 class Directory:
     def __init__(self, year, ip, path):
         self.year = year
         self.ip = ip
         self.path = path
+
+
+def missed_years(ftp_storage):
+    missed = []
+    for dir in ftp_storage.dirs:
+        full_path = os.path.join(ftp_storage.storage_to_dirs[dir.ip], dir.path)
+
+        if not os.path.exists(full_path):
+            missed.append(dir.year)
+
+    return missed
+
+
+def download_missed_files(ftp_storage, temp_dir="../temp_missed/"):
+    if not os.path.exists(temp_dir):
+        os.mkdir(temp_dir)
+
+    missed_years_ = missed_years(ftp_storage)
+    for dir in ftp_storage.dirs:
+        if dir.year in missed_years_:
+            ftp_storage.download_year(dir.year, path_to_download=temp_dir)
--- src/refined.py
+++ src/refined.py
@@ -1,7 +1,12 @@
 from datetime import date
 
 from src.blade import BladeChecker
+from src.ftp import FtpStorage
+from src.ftp import download_missed_files
 
 # checker = BladeChecker(date_from=date(1964, 1, 1), date_to=date(2015, 12, 31))
-checker = BladeChecker(date_from=date(1964, 1, 1), date_to=date(1965, 1, 1))
-checker.check_local_storage(mode="")
+# checker = BladeChecker(date_from=date(1964, 1, 1), date_to=date(1965, 1, 1))
+# checker.check_local_storage(mode="")
+
+storage = FtpStorage()
+download_missed_files(storage)
